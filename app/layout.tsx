41447243ce94dd51e513b5d10073cc7dd9403b9e
Nabeel Asim <nabeelasim0250@gmail.com>
2023-09-09 01:44:38 -0400

    Initial Commit
    - Initiated app with Next.js and Shadcn UI
    - Adjusted indentation from 2 space to 4
    - Added dark/light mode theming with next-themes

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+    title: 'Nabeel Asim - Portfolio',
+    description: 'A portfolio website for showcasing my projects and skills',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={inter.className}>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    )
+}
