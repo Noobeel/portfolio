3fb3206618f3b9a1aad05fe8405c471688a8d028
Nabeel Asim <nabeelasim0250@gmail.com>
2023-10-25 21:46:44 -0400

    Styled Resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,11 +1,7 @@
 export default function Resume() {
     return (
-        <div className="">
-            <p>
-                This is the resume section
-            </p>
-
-            <iframe src='/Nabeel-Resume.pdf' />
+        <div className="w-screen h-screen flex justify-center items-center">
+            <iframe id="resume" title="Resume" src='/Nabeel-Resume.pdf' className="w-[80%] h-[80%] mt-10 rounded-lg" />
         </div>
-      );
+    );
 }
