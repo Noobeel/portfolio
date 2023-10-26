3e8d2f7a2675a035617d6f84415b40f065c5f322
Nabeel Asim <nabeelasim0250@gmail.com>
2023-10-25 21:47:08 -0400

    Moved navbar and socials component to base layout and added toast component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import './globals.css'
+import "@/public/devicon.min.css"
 import type { Metadata } from 'next'
+import { Toaster } from "@/components/ui/toaster"
 import { Montserrat, Poppins } from 'next/font/google'
 import { ThemeProvider } from "@/components/theme-provider"
+import { Navbar, Socials } from '@/components/sections'
 
 const poppins = Poppins({
     weight: ["700"],
@@ -26,10 +29,15 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en" className='snap-y snap-mandatory' suppressHydrationWarning>
+        <html lang="en" suppressHydrationWarning>
             <body className={`${poppins.variable} font-montserrat text-foreground bg-background min-h-screen max-w-full overflow-x-hidden`}>
                 <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+                    <Navbar />
+                    <Socials />
+                    
                     {children}
+
+                    <Toaster />
                 </ThemeProvider>
             </body>
         </html>
