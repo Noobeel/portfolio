92d2c89b9915e6d05583aae108c834fda8311e82
Nabeel Asim <nabeelasim0250@gmail.com>
2023-10-25 21:41:54 -0400

    Moved skills, socials, and navbar components

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,15 +1,11 @@
-import { Navbar, Hero, About, Skills, Projects, Contact, Socials } from '@/components/sections'
+import { Hero, About, Projects, Contact} from '@/components/sections'
 
 export default function Home() {
     return (
         <main className="h-full w-full relative">
-            <Socials />
-            <Navbar />
-
             <div className="flex flex-col w-full h-full">
                 <Hero />
                 <About />
-                <Skills />
                 <Projects />
                 <Contact />
             </div>
