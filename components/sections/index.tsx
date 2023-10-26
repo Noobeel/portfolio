93c5e9177cae526fb86b6eb34b38035e1b937a72
Nabeel Asim <nabeelasim0250@gmail.com>
2023-10-25 21:45:31 -0400

    Removed skills component

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -1,9 +1,8 @@
 import Hero from '@/components/sections/hero'
 import About from '@/components/sections/about'
-import Skills from '@/components/sections/skills'
 import Navbar from '@/components/sections/navbar'
 import Socials from '@/components/sections/socials'
 import Contact from '@/components/sections/contact'
 import Projects from '@/components/sections/projects'
 
-export { Navbar, Hero, About, Skills, Projects, Contact, Socials }
+export { Navbar, Hero, About, Projects, Contact, Socials }
