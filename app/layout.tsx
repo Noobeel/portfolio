79c3af9126bd8413bfbcfbec85662d4a47611f31
Nabeel Asim <nabeelasim0250@gmail.com>
2023-10-27 01:24:16 -0400

    Fixed bug where startup theme was not set

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { Montserrat, Poppins } from 'next/font/google'
 import { ThemeProvider } from "@/components/theme-provider"
 import { Navbar, Socials } from '@/components/sections'
+import { useEffect } from 'react'
 
 const poppins = Poppins({
     weight: ["700"],
@@ -31,7 +32,7 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={`${poppins.variable} font-montserrat text-foreground bg-background min-h-screen max-w-full overflow-x-hidden`}>
-                <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
                     <Navbar />
                     <Socials />
                     
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -7,13 +7,25 @@ import { Button } from "@/components/ui/button";
 export default function DarkModeToggle() {
     const { setTheme } = useTheme();
 
+    const handleChangeTheme = () => {
+        const currentTheme = localStorage.getItem('theme');
+        
+        if (currentTheme === "dark") {
+            setTheme("light");
+        } else if (currentTheme === "light") {
+            setTheme("dark");
+        } else {
+            window.matchMedia('(prefers-color-scheme: dark)').matches ? setTheme("light") : setTheme("dark");
+        }
+    }
+
     return (
         <Button
             variant="outline"
             size="icon"
             className="bg-transparent"
             id="theme-toggle"
-            onClick = { () => localStorage.getItem('theme') === "dark" ? setTheme("light") : setTheme("dark") }
+            onClick = { handleChangeTheme }
         >
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
