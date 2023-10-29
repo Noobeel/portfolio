732a805937b2fbbda71366587342fc759beee42a
Nabeel Asim <nabeelasim0250@gmail.com>
2023-10-29 05:12:27 -0400

    Added check for touch users, fixed card height issue

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -11,9 +11,10 @@ import {
 import { projects } from '@/data/constants';
 
 import { motion, MotionConfig, AnimatePresence } from 'framer-motion';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Projects() {
+    const [isTouchDevice, setIsTouchDevice] = useState(false);
     const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
     const [isFocus, setIsFocus] = useState(false);
 
@@ -29,19 +30,27 @@ export default function Projects() {
         );
     }
 
+    useEffect(() => {
+        setIsTouchDevice('ontouchstart' in window || navigator.maxTouchPoints > 0);
+
+        if (isTouchDevice) {
+            setIsFocus(true);
+        }
+    }, []);
+
     return (
         <section id="projects" className="overflow-hidden">
             <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
-                <div className="flex items-center relative w-3/5">
+                <div className="flex items-center relative w-3/5 h-fit">
                     <AnimatePresence>
                         {isFocus && (
                             <motion.div
                                 className="absolute left-2 right-2 flex justify-between z-10"
-                                initial={{ opacity: 0 }}
+                                initial={isTouchDevice ? { opacity: 1 } : { opacity: 0 }}
                                 animate={{ opacity: 1 }}
                                 exit={{ opacity: 0 }}
                                 onHoverStart={() => setIsFocus(true)}
-                                onHoverEnd={() => setIsFocus(false)}
+                                onHoverEnd={() => isTouchDevice ? null : setIsFocus(false)}
                             >
                                 <button onClick={handleLeftArrowClick} className="flex items-center justify-center w-12 h-12 bg-gray-800 rounded-full shadow-lg focus:outline-none">
                                     <ChevronLeft className="text-gray-100 text-2xl" />
@@ -54,37 +63,41 @@ export default function Projects() {
                     </AnimatePresence>
 
                     <motion.div
-                        className="flex flex-nowrap w-full"
+                        className="flex flex-nowrap w-full h-full"
                         initial={{ x: 0 }}
                         animate={{ x: -currentProjectIndex * 100 + '%' }}
                         onHoverStart={() => setIsFocus(true)}
-                        onHoverEnd={() => setIsFocus(false)}
+                        onHoverEnd={() => isTouchDevice ? null : setIsFocus(false)}
                     >
                         {projects.map((project, index) => (
                             <motion.div
                                 key={index}
                                 animate={{ opacity: index === currentProjectIndex ? 1 : 0.3, scale: index === currentProjectIndex ? 1 : 0.9 }}
-                                className="flex flex-col items-center justify-center rounded-lg shadow-lg min-w-full"
+                                className="flex flex-col items-center justify-center rounded-lg min-w-full min-h-full bg-gray-800"
                             >
-                                <div className="flex items-center justify-center rounded-t-lg">
+                                <div className="flex justify-center overflow-y-hidden flex-1">
                                     <Image
                                         src={project.image}
                                         alt={project.title}
                                         width="0"
                                         height="0"
-                                        sizes="100vw"
+                                        sizes="100vh"
+                                        priority={true}
+                                        className="object-contain"
                                         style={{
-                                            width: '80%',
-                                            height: 'auto',
+                                            width: '90%',
+                                            height: '100%'
                                         }}
                                     />
                                 </div>
 
-                                <div className="flex flex-col w-full h-full p-4 bg-gray-800 rounded-b-lg">
-                                    <h1 className="text-2xl font-bold text-gray-100">{project.title}</h1>
-                                    <p className="text-gray-300">{project.description}</p>
+                                <div className="flex flex-col self-end w-full px-4 pb-4 rounded-b-lg h-fit">
+                                    <div className="flex flex-col items-center">
+                                        <h1 className="text-2xl font-bold text-gray-100">{project.title}</h1>
+                                        <p className="text-gray-300 mx-2">{project.description}</p>
+                                    </div>
                                     
-                                    <div className="flex flex-row items-center justify-between w-max mt-4">
+                                    <div className="flex flex-row items-center justify-between w-max mt-4 mx-2">
                                         {project.github && (
                                             <TooltipProvider delayDuration={0}>
                                                 <Tooltip>
