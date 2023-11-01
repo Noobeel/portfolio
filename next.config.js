f89911585051f406a2b57a36f0f367a2a79d0c2d
Nabeel Asim <nabeelasim0250@gmail.com>
2023-11-01 02:58:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,45 +3,12 @@ const nextConfig = {
     async redirects() {
         return [
             {   
-                has: [
-                    {
-                        type: 'host',
-                        value: 'resume.nabeelasim.net',
-                    },
-                ],
-                source: '/',
-                destination: '/resume',
-                permanent: true,
-            },
-            {
-                has: [
-                    {
-                        type: 'host',
-                        value: 'nabeelasim.net',
-                    },
-                ],
                 source: '/resume',
                 destination: 'https://resume.nabeelasim.net',
                 permanent: true,
             },
         ]
     },
-
-    async rewrites() {
-        return [
-            {
-                has: [
-                    {
-                        type: 'host',
-                        value: 'resume.nabeelasim.net',
-                    },
-                ],
-                source: '/resume',
-                destination: '/',
-            },
-        ]
-    }
-
 }
 
 module.exports = nextConfig
