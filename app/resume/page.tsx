8757979bde5825371c6e97b235f2ba59aa2c2330
Nabeel Asim <nabeelasim0250@gmail.com>
2023-11-16 00:11:12 -0500

    Made responsive

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,8 +1,7 @@
 export default function Resume() {
-    
     return (
         <div className="w-screen h-screen flex justify-center items-center">
-            <iframe id="resume" title="Resume" src='/Nabeel-Resume.pdf' className="w-[80%] h-[80%] mt-10 rounded-lg" />
+            <iframe id="resume" title="Resume" src='/Nabeel-Resume.pdf' className="mt-10 rounded-lg w-5/6 h-5/6 lg:w-4/6" />
         </div>
     );
 }
