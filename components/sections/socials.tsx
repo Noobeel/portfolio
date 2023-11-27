67bc9c6804252f4836b873130662e862f706ba25
Nabeel Asim <nabeelasim0250@gmail.com>
2023-11-27 00:03:00 -0500

    Made socials component background lighter

--- components/sections/socials.tsx
+++ components/sections/socials.tsx
@@ -75,7 +75,7 @@ export default function Socials() {
                 transition={{ duration: 1, ease: [0.32, 0.72, 0, 1] }}
                 className="fixed right-0 z-20 flex items-start mt-20 h-max sm:items-center sm:h-screen sm:mt-0"
             >
-                <div className="flex flex-col justify-center items-center w-max h-max backdrop-blur-md rounded-l-lg bg-gradient-to-r from-gray-300 to-[#b0b0b0] dark:from-gray-900 dark:to-slate-800">
+                <div className="flex flex-col justify-center items-center w-max h-max backdrop-blur-md rounded-l-lg bg-gradient-to-r from-gray-300 to-[#c4c4c4] dark:from-gray-900 dark:to-slate-800">
                     {socials.map((social, index) => (
                         <TooltipProvider key={index} delayDuration={0}>
                             <Tooltip>
