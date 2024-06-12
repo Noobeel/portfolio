75d9ce7da7a506047c476c3fb50f512279470610
Nabeel Asim <nabeelasim0250@gmail.com>
2024-06-11 23:39:36 -0400

    Removed margin from loading icon

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -231,7 +231,7 @@ export default function Contact() {
                                     className="w-max mx-auto px-10 font-semibold text-base"
                                     disabled
                                 >
-                                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                    <Loader2 className="h-4 w-4 animate-spin" />
                                 </Button>
                             ) : (
                                 <Button
